f676676dd977277e85c97fb0c95ee0cf9c736d76
Simon <simon.naud@icoud.com>
2024-03-06 13:36:41 +0100

    first commit

---
+++ GitProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitProject
+//
+//  Created by Simon Naud on 06/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitProject/GitProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitProjectApp.swift
+//  GitProject
+//
+//  Created by Simon Naud on 06/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
